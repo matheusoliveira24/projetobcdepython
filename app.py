c6ac1b50793f3b15e7d6ec36d2609caecb87e3a6
Victor Vicentini Cunha <159261514+VictorCunha26@users.noreply.github.com>
2024-11-22 16:36:36 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+from modelos.avengers import Avengers
+from modelos.interface import Interface as i
+
+
+def main():
+   i.apresentar_menu_principal()
+if __name__ == '__main__': # sempre no final do arquivo contendo a definição da função principal
+    main()
