d60c0da2051b517c54562b1ada98705324e5d612
Victor Vicentini Cunha <159261514+VictorCunha26@users.noreply.github.com>
2024-11-27 16:34:54 -0300

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from modelos.avengers import Avengers
-from modelos.interface import Interface as i
-
-
-def main():
-   i.apresentar_menu_principal()
-if __name__ == '__main__': # sempre no final do arquivo contendo a definição da função principal
-    main()
